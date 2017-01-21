7c39f24ceed22042ef5b394b83db36746961ab02
DarkyZShadow <s.soulie@hotmail.fr>
2017-01-21 22:42:20 +0100

    Added '/packages' route

--- Server/controllers/routes/packages.js
+++ Server/controllers/routes/packages.js
@@ -5,7 +5,18 @@
 module.exports = {
     GET: function (req, res)
     {
-        res.sendStatus(404);
+        var filePath = req.originalUrl;
+
+        if (fs.existsSync(__dirname + filePath))
+        {
+            console.log('[LOG] Successfully downloaded package : ' + filePath);
+            res.sendFile(filePath, {root: __dirname });
+        }
+        else
+        {
+            console.log('[ERROR] Package file not found : ' + filePath);
+            res.sendStatus(404);
+        }
     }
 }
 
