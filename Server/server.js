d332b17ce478ac855e542e3329afce9b15fb30bc
Steve <theval_s@etna-alternance.net>
2017-01-31 18:01:15 +0100

    Package & Server posts

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,52 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var compareVersions = require('compare-versions');
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'prep_get'
+});
+
 var app = express();
 
 const port = 4242;
 
 app.use(bodyParser.json())
 
-app.post('/test', function (req, res) {
+.post('/install', function (req, res) {
+    if (req.body.packets) {
+        console.log("POST /");
+        res.end(JSON.stringify(req.body));
+    }
+    else {
+        res.sendStatus(403);
+    }
+})
+
+.post('/search', function (req, res) {
+    if (req.body.packets) {
+        console.log("POST /");
+        res.end(JSON.stringify(req.body));
+    }
+    else {
+        res.sendStatus(403);
+    }
+})
+
+.post('/upload', function (req, res) {
+    if (req.body.packets) {
+        console.log("POST /");
+        res.end(JSON.stringify(req.body));
+    }
+    else {
+        res.sendStatus(403);
+    }
+})
+
+.post('/list', function (req, res) {
     if (req.body.packets) {
         console.log("POST /");
         res.end(JSON.stringify(req.body));
