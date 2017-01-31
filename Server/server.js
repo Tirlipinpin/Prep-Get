568f3d376d81912ae61c6baa889a41ee6c6d2999
Steve <theval_s@etna-alternance.net>
2017-01-31 16:48:42 +0100

    POST Node

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,24 @@
-const http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
 const port = 4242;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.use(bodyParser.json())
 
-server.listen(port, () => {
-  console.log(`Server running at http://127.0.0.1:${port}/`);
-});
+app.post('/test', function (req, res) {
+    if (req.body.packets) {
+        console.log("POST /");
+        res.end(JSON.stringify(req.body));
+    }
+    else {
+        res.sendStatus(403);
+    }
+})
+
+.use(function (req, res) {
+    res.sendStatus(403);
+})
+
+.listen(port);
+console.log('Listening at http://localhost:' + port)
