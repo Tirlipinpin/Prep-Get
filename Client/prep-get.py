96b90e3310ac13dafc6120d62ceb39444c83ab52
Gaudeah <gaudea_h@etna-alternance.net>
2017-02-01 11:00:47 +0100

    get url, download file

--- Client/prep-get.py
+++ Client/prep-get.py
@@ -2,8 +2,10 @@
 import json
 import argparse
 import sys
+import os
 from urllib import request
-URL = "http://172.16.1.99:4242/install"
+ROOT = "http://172.16.1.99:4242"
+URL = ROOT + "/install"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('install', nargs='+', help='install a package')
@@ -24,6 +26,17 @@ def main():
         
     params = json.dumps(data).encode('utf8')
     req = request.Request(URL, data=params, headers={'content-type': 'application/json'})
-    response = request.urlopen(req)
+    response = request.urlopen(req).read().decode("utf8")
+    files = json.loads(response)
+    if not os.path.exists("tmp") :
+        os.makedirs("tmp")
+    print(files)
+    for file in files :
+        request.urlretrieve(ROOT + file.url, "tmp/" + file.name + ".tar.gz")
+    if os.listdir("tmp") == [] :
+        print("Error while downloading packages")
+    else :
+        print("Packages successfully downloaded")
+    os.rmdir("tmp")
     
 main()
