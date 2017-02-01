d9ac3bb10e7045daa5dd92d12da6c045b0346296
nicolas-law <lawyim_n@etna-alternance.net>
2017-02-01 11:26:51 +0100

    Updating code

--- Client/prep-get.py
+++ Client/prep-get.py
@@ -2,8 +2,10 @@
 import json
 import argparse
 import sys
+import os
 from urllib import request
-URL = "http://172.16.1.99:4242/install"
+ROOT = "http://172.16.1.99:4242"
+URL = ROOT + "/install"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('install', nargs='+', help='install a package')
@@ -11,17 +13,30 @@ args = parser.parse_args()
 
 if sys.argv[1] == 'install' and sys.argv[2: ]:
     for _, value in parser.parse_args()._get_kwargs():
-        print("ok")
+        print("Packages sended")
 else :
     print("No package(s) selected")
 
 def main():
     count = 1
+    data = {"packages" :[]}
     while count < len(value):
-        data = {"packets" :[{"name": value[count],"current_version": "2.0.0", "version": "2.0.1"}]}
-        params = json.dumps(data).encode('utf8')
-        req = request.Request(URL, data=params, headers={'content-type': 'application/json'})
-        response = request.urlopen(req)
+        data["packages"].append({"name": value[count]})
         count += 1
+        
+    params = json.dumps(data).encode('utf8')
+    req = request.Request(URL, data=params, headers={'content-type': 'application/json'})
+    response = request.urlopen(req).read().decode("utf8")
+    files = json.loads(response)
+    if not os.path.exists("tmp") :
+        os.makedirs("tmp")
+    print(files)
+    for file in files :
+        request.urlretrieve(ROOT + file.url, "tmp/" + file.name + ".tar.gz")
+    if os.listdir("tmp") == [] :
+        print("Error while downloading packages")
+    else :
+        print("Packages successfully downloaded")
+    os.rmdir("tmp")
     
 main()
