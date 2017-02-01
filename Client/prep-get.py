45c31048d9573b0a89aba8f3262769281ea1a698
Gaudeah <gaudea_h@etna-alternance.net>
2017-02-01 15:19:28 +0100

    etape 2 du client OK

--- Client/prep-get.py
+++ Client/prep-get.py
@@ -7,20 +7,10 @@ import shutil
 from urllib import request
 
 ROOT = "http://172.16.1.99:4242"
-URL = ROOT + "/install"
 
-parser = argparse.ArgumentParser()
-parser.add_argument('install', nargs='+', help='install a package')
-args = parser.parse_args()
-
-if sys.argv[1] == 'install' and sys.argv[2: ]:
-    for _, value in parser.parse_args()._get_kwargs():
-        print("Packages sent")
-else :
-    print("No package(s) selected")
-
-def main():
-    count = 1
+def install():
+    URL = ROOT + "/install"
+    count = 0
     data = {"packages" :[]}
     while count < len(value):
         data["packages"].append({"name": value[count]})
@@ -39,7 +29,34 @@ def main():
         print("Error while downloading packages")
     else :
         print("Packages successfully downloaded")
+    if os.path.exists("tmp_pack") :
+        shutil.rmtree("tmp_pack", ignore_errors=True)
     
-main()
-# if os.path.exists("tmp_pack") :
-#     shutil.rmtree("tmp_pack", ignore_errors=True)
+def search():
+    URL = ROOT + "/search"
+    print("Checking database for :", value[0])
+    data = {"search": value[0]}
+    params = json.dumps(data).encode('utf8')
+    req = request.Request(URL, data=params, headers={'content-type': 'application/json'})
+    response = request.urlopen(req).read().decode("utf8")
+    files = json.loads(response)
+    for obj in files:
+        print("Package found : ", obj['name'])
+
+parser = argparse.ArgumentParser()
+parser.add_argument('install', nargs='+', help='install a package')
+parser.add_argument('search', nargs='+', help='check if package exists')
+args = parser.parse_args()
+
+if sys.argv[1] == 'install' and sys.argv[2: ]:
+    for _, value in parser.parse_args()._get_kwargs():
+        pass
+        # print("Checking database for :", value)
+    install()
+elif sys.argv[1] == 'search' and sys.argv[2: ]:
+    for _, value in parser.parse_args()._get_kwargs():
+        pass
+        # print("Searching package :", value)
+    search()
+else:
+    print("No package(s) selected")
