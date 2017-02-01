a1641fa1bc1eaabe94df9e22ea06adb447506edb
Gaudeah <gaudea_h@etna-alternance.net>
2017-02-01 17:19:00 +0100

    correct plusieurs arguments

--- Client/prep-get.py
+++ Client/prep-get.py
@@ -10,12 +10,12 @@ ROOT = "http://172.16.1.99:4242"
 
 def install():
     URL = ROOT + "/install"
-    count = 0
+    count = 1
     data = {"packages" :[]}
     while count < len(value):
         data["packages"].append({"name": value[count]})
         count += 1
-        
+
     params = json.dumps(data).encode('utf8')
     req = request.Request(URL, data=params, headers={'content-type': 'application/json'})
     response = request.urlopen(req).read().decode("utf8")
@@ -29,8 +29,8 @@ def install():
         print("Error while downloading packages")
     else :
         print("Packages successfully downloaded")
-    if os.path.exists("tmp_pack") :
-        shutil.rmtree("tmp_pack", ignore_errors=True)
+    # if os.path.exists("tmp_pack") :
+    #     shutil.rmtree("tmp_pack", ignore_errors=True)
     
 def search():
     URL = ROOT + "/search"
@@ -45,7 +45,7 @@ def search():
 
 parser = argparse.ArgumentParser()
 parser.add_argument('install', nargs='+', help='install a package')
-parser.add_argument('search', nargs='+', help='check if package exists')
+# parser.add_argument('search', nargs='+', help='check if package exists')
 args = parser.parse_args()
 
 if sys.argv[1] == 'install' and sys.argv[2: ]:
