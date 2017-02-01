8cdd0f37f0068d987bb3559a32e5e2d3b2e69b05
Gaudeah <gaudea_h@etna-alternance.net>
2017-02-01 09:46:00 +0100

    envoi donnees url

--- Client/prep-get.py
+++ Client/prep-get.py
@@ -11,17 +11,19 @@ args = parser.parse_args()
 
 if sys.argv[1] == 'install' and sys.argv[2: ]:
     for _, value in parser.parse_args()._get_kwargs():
-        print("ok")
+        print("Packages sended")
 else :
     print("No package(s) selected")
 
 def main():
     count = 1
+    data = {"packages" :[]}
     while count < len(value):
-        data = {"packets" :[{"name": value[count],"current_version": "2.0.0", "version": "2.0.1"}]}
-        params = json.dumps(data).encode('utf8')
-        req = request.Request(URL, data=params, headers={'content-type': 'application/json'})
-        response = request.urlopen(req)
+        data["packages"].append({"name": value[count]})
         count += 1
+        
+    params = json.dumps(data).encode('utf8')
+    req = request.Request(URL, data=params, headers={'content-type': 'application/json'})
+    response = request.urlopen(req)
     
 main()
