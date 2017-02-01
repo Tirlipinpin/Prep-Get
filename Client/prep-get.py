8a96acf29128736ce41610461b70f97c95e68b70
Gaudeah <gaudea_h@etna-alternance.net>
2017-02-01 11:49:33 +0100

    suppression du dossier tmp

--- Client/prep-get.py
+++ Client/prep-get.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 
 if sys.argv[1] == 'install' and sys.argv[2: ]:
     for _, value in parser.parse_args()._get_kwargs():
-        print("Packages sended")
+        print("Packages sent")
 else :
     print("No package(s) selected")
 
@@ -28,15 +28,16 @@ def main():
     req = request.Request(URL, data=params, headers={'content-type': 'application/json'})
     response = request.urlopen(req).read().decode("utf8")
     files = json.loads(response)
-    if not os.path.exists("tmp") :
-        os.makedirs("tmp")
+    if not os.path.exists("tmp_pack") :
+        os.makedirs("tmp_pack")
     print(files)
     for file in files :
-        request.urlretrieve(ROOT + file.url, "tmp/" + file.name + ".tar.gz")
-    if os.listdir("tmp") == [] :
+        request.urlretrieve(ROOT + file.url, "tmp_pack/" + file.name + ".tar.gz")
+    if os.listdir("tmp_pack") == [] :
         print("Error while downloading packages")
     else :
         print("Packages successfully downloaded")
-    os.rmdir("tmp")
     
 main()
+if os.path.exists("tmp_pack") :
+    os.rmdir("tmp_pack")
