9ea03b77d784e56db940b9e20aed8ea7db57061b
nicolas-law <lawyim_n@etna-alternance.net>
2017-02-01 15:19:18 +0100

    Etape 1 & 2 working

--- Client/prep-get.py
+++ Client/prep-get.py
@@ -6,28 +6,30 @@ import os
 import shutil
 from urllib import request
 
+ROOT = "http://172.16.1.99:4242"
+
 def install():
-    count = 1
+    URL = ROOT + "/install"
+    count = 0
     data = {"packages" :[]}
     while count < len(value):
         data["packages"].append({"name": value[count]})
         count += 1
-        
     params = json.dumps(data).encode('utf8')
     req = request.Request(URL, data=params, headers={'content-type': 'application/json'})
     response = request.urlopen(req).read().decode("utf8")
     files = json.loads(response)
     if not os.path.exists("tmp_pack") :
         os.makedirs("tmp_pack")
-    print(files)
     for file in files :
-        request.urlretrieve(ROOT + file.url, "tmp_pack/" + file.name + ".tar.gz")
+        request.urlretrieve(ROOT + file["url"], "tmp_pack/" + file["name"] + ".tar.gz")
     if os.listdir("tmp_pack") == [] :
         print("Error while downloading packages")
     else :
         print("Packages successfully downloaded")
     
 def search():
+    URL = ROOT + "/search"
     print("Checking database for :", value[0])
     data = {"search": value[0]}
     params = json.dumps(data).encode('utf8')
@@ -36,8 +38,6 @@ def search():
     files = json.loads(response)
     for obj in files:
         print("Package found : ", obj['name'])
-ROOT = "http://172.16.1.99:4242"
-URL = ROOT + "/search"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('install', nargs='+', help='install a package')
@@ -47,15 +47,10 @@ args = parser.parse_args()
 if sys.argv[1] == 'install' and sys.argv[2: ]:
     for _, value in parser.parse_args()._get_kwargs():
         pass
-        # print("Checking database for :", value)
     install()
 elif sys.argv[1] == 'search' and sys.argv[2: ]:
     for _, value in parser.parse_args()._get_kwargs():
         pass
-        #print("Searching package :", value)
     search()
 else:
     print("No package(s) selected")
-
-if os.path.exists("tmp_pack") :
-    shutil.rmtree("tmp_pack", ignore_errors=True)
