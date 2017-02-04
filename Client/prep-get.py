31358b014a2e577a64a109ca754be6ce673ee46b
Gaudeah <gaudea_h@etna-alternance.net>
2017-02-04 09:37:28 +0100

    update upload

--- Client/prep-get.py
+++ Client/prep-get.py
@@ -4,10 +4,28 @@ import argparse
 import sys
 import os
 import shutil
-from urllib.request import urlopen
+import re
 from urllib import request
 
-ROOT = "http://172.16.1.99:4242"
+ROOT = "http://172.16.1.74:4242"
+
+def upload(path) :
+    URL = ROOT + "/upload"
+    if os.path.isfile(path) :
+        if re.search('([^/]+)_([^_]+).tar.gz$', path):
+            param = re.findall('([^/]+)_([^_]+).tar.gz$', '/tmp_pack/mysql-5.6_5.6.35.tar.gz')
+            with open(path, "rb") as f:
+                byte = f.read()
+                req = request.Request(URL, data=byte, headers={'content-type': 'application/octet-stream'})
+                req.add_header('package_name', param[0][0])
+                req.add_header('package_version', param[0][1])
+                req.add_header('Content-Length', '%d'% len(byte))
+                response = request.urlopen(req).read().decode("utf8")
+                print("File uploaded correctly")
+        else:
+            print("Bad name for the file, expected [PACKAGE]_[VERSION].tar.gz")
+    else :
+        print(path + ": No such file or directory")
 
 def install():
     URL = ROOT + "/install"
@@ -26,9 +44,10 @@ def install():
     if not os.path.exists("tmp_pack") :
         os.makedirs("tmp_pack")
     for file in files :
+        print(file)
         if "url" in file :
             print("Downloading " + file["name"])
-            request.urlretrieve(ROOT + file["url"], "tmp_pack/" + file["name"] + ".tar.gz")
+            request.urlretrieve(ROOT + file["url"], "tmp_pack/" + file["name"] + "_" + file["version"] + ".tar.gz")
         else :
             print("Sorry, the package " + file["name"] + " doesn\'t exist")
             check += 1
@@ -53,7 +72,6 @@ def search():
 
 parser = argparse.ArgumentParser()
 parser.add_argument('install', nargs='+', help='install a package')
-# parser.add_argument('search', nargs='+', help='check if package exists')
 args = parser.parse_args()
 
 if sys.argv[1] == 'install' and sys.argv[2: ]:
@@ -64,7 +82,9 @@ elif sys.argv[1] == 'search' and sys.argv[2: ]:
     for _, value in parser.parse_args()._get_kwargs():
         pass
     search()
-elif sys.argv[1] != 'install' and sys.argv[1] != 'search':
+elif sys.argv[1] == 'upload' and sys.argv[2: ]:
+    upload(sys.argv[2])
+elif sys.argv[1] != 'install' and sys.argv[1] != 'search' and sys.argv[1] != 'upload':
     print("Bad entry, please consult the help")
 else :
     print("No package(s) selected");
