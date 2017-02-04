e1447d9ad14fc8dad764bf33b00b5082b487a6c8
Gaudeah <gaudea_h@etna-alternance.net>
2017-02-04 09:49:41 +0100

    maj du client, systeme d upload ok, search et install maj

--- Client/prep-get.py
+++ Client/prep-get.py
@@ -9,7 +9,7 @@ from urllib import request
 
 ROOT = "http://172.16.1.74:4242"
 
-def upload(path) :
+def upload_func(path) :
     URL = ROOT + "/upload"
     if os.path.isfile(path) :
         if re.search('([^/]+)_([^_]+).tar.gz$', path):
@@ -27,37 +27,37 @@ def upload(path) :
     else :
         print(path + ": No such file or directory")
 
-def install():
+def install_func():
     URL = ROOT + "/install"
     check = 0
-    count = 1
+    count = 0
     data = {"packages" :[]}
     while count < len(value):
-        data["packages"].append({"name": value[count]})
+        if '=' in value[count]:
+            tmp = value[count].split("=")
+            data["packages"].append({"name": tmp[0], "version": tmp[1]})
+        else:
+            data["packages"].append({"name": value[count]})            
         count += 1
-
     params = json.dumps(data).encode('utf8')
     req = request.Request(URL, data=params, headers={'content-type': 'application/json'})
     response = request.urlopen(req).read().decode("utf8")
     files = json.loads(response)
-
     if not os.path.exists("tmp_pack") :
         os.makedirs("tmp_pack")
     for file in files :
-        print(file)
         if "url" in file :
             print("Downloading " + file["name"])
-            request.urlretrieve(ROOT + file["url"], "tmp_pack/" + file["name"] + "_" + file["version"] + ".tar.gz")
+            request.urlretrieve(ROOT + file["url"], "tmp_pack/" + file["name"] + ".tar.gz")
         else :
             print("Sorry, the package " + file["name"] + " doesn\'t exist")
             check += 1
-
     if check != 0 :
         print("Error while downloading some packages")
     else :
         print("Everything has been successfully downloaded")
     
-def search():
+def search_func():
     URL = ROOT + "/search"
     count = 1
     while count < len(value):
@@ -71,19 +71,25 @@ def search():
         print("Package found : ", obj['name'])
 
 parser = argparse.ArgumentParser()
-parser.add_argument('install', nargs='+', help='install a package')
-args = parser.parse_args()
+subparser = parser.add_subparsers()
+install = subparser.add_parser('install', help='install a package')
+install.add_argument('package', nargs='+', help='package name')
+search = subparser.add_parser('search', help='check if package exists')
+search.add_argument('search', nargs='+', help='check if package exists')
+upload = subparser.add_parser('upload', help='upload a package to the server')
+upload.add_argument('upload', nargs='+', help='upload a package to the server')
+args = parser.parse_args()._get_kwargs()
 
 if sys.argv[1] == 'install' and sys.argv[2: ]:
-    for _, value in parser.parse_args()._get_kwargs():
+    for _, value in args:
         pass
-    install()
+    install_func()
 elif sys.argv[1] == 'search' and sys.argv[2: ]:
-    for _, value in parser.parse_args()._get_kwargs():
+    for _, value in args:
         pass
-    search()
+    search_func()
 elif sys.argv[1] == 'upload' and sys.argv[2: ]:
-    upload(sys.argv[2])
+    upload_func(sys.argv[2])
 elif sys.argv[1] != 'install' and sys.argv[1] != 'search' and sys.argv[1] != 'upload':
     print("Bad entry, please consult the help")
 else :
