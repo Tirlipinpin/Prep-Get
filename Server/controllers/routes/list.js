3256cfe715031ae1c65eac5ddb9847de85e10398
DarkyZShadow <s.soulie@hotmail.fr>
2017-02-05 13:36:48 +0100

    Modified 'list.js'

--- Server/controllers/routes/list.js
+++ Server/controllers/routes/list.js
@@ -8,12 +8,28 @@ var connection = require("../../config/db.js").connection;
 module.exports = {
     GET: function (req, res)
     {
-        connection.query('SELECT name FROM packages ORDER BY name ASC', function(err, rows, fields) {
-            if (!err && rows[0] != undefined) {
-                logger.log(1, 'List successfully append');
-                res.end(JSON.stringify(rows));
+        connection.query('SELECT name, version FROM packages_versions JOIN packages ON id = package_id', function(err, rows, fields) {
+            if (!err && rows[0] != undefined)
+			{
+                var obj_name = ""
+				var result = [];
+				for (index in rows)
+				{
+					if (obj_name === "" || rows[index].name !== result[result.length - 1].name)
+					{
+						obj_name = rows[index].name;
+						result.push({ name: obj_name, versions: [rows[index].version] });
+					}
+					else if (rows[index].name === result[result.length - 1].name)
+					{
+						result[result.length - 1].versions.push(rows[index].version);
+					}
+				}
+				logger.log(1, 'List successfully append');
+				res.end(JSON.stringify(result));
             }
-            else {
+            else
+			{
                 logger.log(0, 'Cannot get all packages by /list');
                 res.sendStatus(404);
             }
